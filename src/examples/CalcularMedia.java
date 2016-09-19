ceb3a7a6a32608a7ab1f94bef955efccd40dcefe
IR012982DG <IR012982DG@H012982.NET>
2016-09-19 13:08:02 +0200

    m

---
+++ src/examples/CalcularMedia.java
@@ -0,0 +1,28 @@
+package examples;
+
+import java.util.Scanner;
+
+public class CalcularMedia {
+
+	public static void main(String[] args) {
+
+		int x,y;
+		double z;
+		
+		// https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("x:");
+		x = sc.nextInt();
+		x=6;
+		y=5;
+		z = (double)(x+y)/2;
+		
+		System.out.println("z:" + z);
+		
+		System.out.format("z:%.2f", z);  // two decimals
+		
+		sc.close();
+	}
+
+}
